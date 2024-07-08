fe42cde14793e1c002beb08ba474635e31b7764b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-08 19:09:22 -0300

    feat: bottom tabbed bar controller

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fromnotes_mobile/pages/home/internal/about_page.dart';
+import 'package:fromnotes_mobile/pages/home/internal/notes_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int index = 0;
+
+  List<Widget> screens = const [NotesPage(), AboutPage()];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Inicio'),
+      ),
+      body: PopScope(
+        canPop: false,
+        child: IndexedStack(
+          index: index,
+          children: screens,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (newIndex) {
+          setState(() {
+            index = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.description_outlined), label: 'Inicio'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.info_outline), label: 'Sobre')
+        ],
+      ),
+    );
   }
 }
