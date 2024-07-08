e63e5eecb3cda71cf4af4c15d95bd4e909ba3b9b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-08 19:55:46 -0300

    feat: about application page

--- lib/pages/home/internal/about_page.dart
+++ lib/pages/home/internal/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({super.key});
@@ -10,6 +11,26 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            'lib/assets/images/logo.png',
+            height: 150,
+          ),
+          const Text(
+            'FromHel iSync',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
+          ),
+          const Text('Versão 0.0.1'),
+          const SizedBox(
+            height: 25,
+          ),
+          const Text('Desenvolvido por Gabriel Araújo e Matheus Abrantes.'),
+        ],
+      ),
+    );
   }
 }
