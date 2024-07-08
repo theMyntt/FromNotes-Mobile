122da3a4c079f6703fcda6e23ea5a816989ac545
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-07-08 18:28:50 -0300

    feat: loader when is making login

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -18,16 +18,24 @@ class _LoginPageState extends State<LoginPage> {
 
   final env = Env();
 
+  bool isLoading = false;
+
   Future<void> makeLogin(BuildContext context) async {
     final apiUrl = env.apiUrl;
 
     try {
+      setState(() {
+        isLoading = true;
+      });
       var response = await post(Uri.parse("$apiUrl/v1/user/sync-out"), body: {
         'email': _email.text.toString(),
         'password': _password.text.toString()
       });
 
       if (response.statusCode != 201) {
+        setState(() {
+          isLoading = false;
+        });
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Usuário não encontrado')),
         );
@@ -41,7 +49,14 @@ class _LoginPageState extends State<LoginPage> {
       prefs.setString('token', jsonResponse['id']);
 
       Navigator.pushNamed(context, '/home');
+
+      setState(() {
+        isLoading = false;
+      });
     } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Erro durante o login')),
       );
@@ -51,46 +66,60 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Faça login para continuar!'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Center(
-          child: Wrap(
-            runSpacing: 20.0,
-            children: [
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                const Text('Digite seu email: '),
-                TextField(
-                  controller: _email,
-                  textCapitalization: TextCapitalization.none,
-                  autocorrect: false,
-                  enableSuggestions: false,
-                )
-              ]),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Digite sua senha: '),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                  )
-                ],
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: TextButton(
-                    onPressed: () => makeLogin(context),
-                    child: const Text('Fazer login')),
-              )
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text('Faça login para continuar!'),
         ),
-      ),
-    );
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Center(
+                child: Wrap(
+                  runSpacing: 20.0,
+                  children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Digite seu email: '),
+                          TextField(
+                            controller: _email,
+                            textCapitalization: TextCapitalization.none,
+                            autocorrect: false,
+                            enableSuggestions: false,
+                          )
+                        ]),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Digite sua senha: '),
+                        TextField(
+                          controller: _password,
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: TextButton(
+                          onPressed: () => makeLogin(context),
+                          child: const Text('Fazer login')),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            if (isLoading)
+              Positioned.fill(
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              ),
+          ],
+        ));
   }
 }
